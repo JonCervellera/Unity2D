61519243e26eaef34a7119e7417011fd32dd4979
JonCervellera <32324044+JonCervellera@users.noreply.github.com>
2017-10-22 14:08:04 +0200

    CollsionDetections (Wall, Ground, Ceiling).
    I just put the collisions.

---
+++ Assets/Scripts/CollisionDetectionCeiling.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetectionCeiling : MonoBehaviour
+{
+    [Header("State")]
+    public bool isCeiled;
+    public bool wasCeiledLastFrame;
+    public bool justCeiled;
+    public bool justNOTCeiled;
+    public bool isJumping;
+
+
+    [Header("Boxes")]
+    public Vector2 ceilingBoxPos;
+    public Vector2 ceilingBoxSize;
+
+    [Header("Properties")]
+    public int maxHits = 1;
+    public bool detectCeiling = true;
+    public ContactFilter2D filter;
+
+
+    private void FixedUpdate()
+    {
+        ResetState();
+        DetectCeiling();
+    }
+
+    void ResetState()
+    {
+        wasCeiledLastFrame = isCeiled;
+        isCeiled = false;
+        justNOTCeiled = false;
+        justCeiled = false;
+        isJumping = true;
+    }
+
+    void DetectCeiling()
+    {
+        if (!detectCeiling) return;
+
+        Collider2D[] results = new Collider2D[maxHits];
+        Vector3 newPos = (Vector3)ceilingBoxPos + transform.position;
+        int numHits = Physics2D.OverlapBox(newPos, ceilingBoxSize, 0, filter, results);
+
+        if (numHits > 0)
+        {
+            isCeiled = true;
+        }
+        isJumping = !isCeiled;
+
+        if (!wasCeiledLastFrame && isCeiled)
+        {
+            Debug.Log("JUST CEILED");
+            justCeiled = true;
+        }
+        if (wasCeiledLastFrame && !isCeiled)
+        {
+            Debug.Log("JUST NOT CEILED");
+            justNOTCeiled = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 newPos = (Vector3)ceilingBoxPos + transform.position;
+        Gizmos.DrawWireCube(newPos, ceilingBoxSize);
+    }
+}
---
+++ Assets/Scripts/CollisionDetectionWall.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetectionWall : MonoBehaviour
+{
+    [Header("State")]
+    public bool isWalled;
+    public bool wasWalledLastFrame;
+    public bool justWalled;
+    public bool justNOTWalled;
+    public bool isWalking;
+
+    [Header("Boxes")]
+    public Vector2 wallBoxPos;
+    public Vector2 wallBoxSize;
+
+    [Header("Properties")]
+    public int maxHits = 1;
+    public bool detectWall = true;
+    public ContactFilter2D filter;
+
+
+    private void FixedUpdate()
+    {
+        ResetState();
+        DetectWall();
+    }
+
+    void ResetState()
+    {
+        wasWalledLastFrame = isWalled;
+        isWalled = false;
+        justNOTWalled = false;
+        justWalled = false;
+        isWalking = true;
+    }
+
+    void DetectWall()
+    {
+        if (!detectWall) return;
+
+        Collider2D[] results = new Collider2D[maxHits];
+        Vector3 newPos = (Vector3)wallBoxPos + transform.position;
+        int numHits = Physics2D.OverlapBox(newPos, wallBoxSize, 0, filter, results);
+
+        if (numHits > 0)
+        {
+            isWalled = true;
+        }
+        isWalking = !isWalled;
+
+        if (!wasWalledLastFrame && isWalled)
+        {
+            Debug.Log("JUST HIT THE WALL");
+            justWalled = true;
+        }
+        if (wasWalledLastFrame && !isWalled)
+        {
+            Debug.Log("JUST NOT HIT THE WALL");
+            justNOTWalled = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 newPos = (Vector3)wallBoxPos + transform.position;
+        Gizmos.DrawWireCube(newPos, wallBoxSize);
+    }
+}
